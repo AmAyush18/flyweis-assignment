570d77f03112d9980edf8d34f8485730f95b9726
Ayush Dwivedi <amayush0818@gmail.com>
2025-01-25 16:18:55 +0530

    Initialization Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-4xl bg-yellow-600 py-6 text-center font-semibold'>
+      Flyweis Assignment
+    </div>
+  )
+}
+
+export default App
